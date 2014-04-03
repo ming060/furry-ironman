055320d840bb5d3ecf9c61afa1daa34ec83478c2
ming060 <lym060@gmail.com>
2014-04-03 20:49:46 +0800

    add some action

--- MyPython/ming/Mobile.py
+++ MyPython/ming/Mobile.py
@@ -1,45 +1,203 @@
 # -*- coding: utf-8 -*-
 from robot.api import logger
-from uiautomator import Device
+from uiautomator import Device, device
 
 class Mobile():
 
-    def __init__(self, serial_number):
-        self.device = Device(serial_number)
+    def __init__(self, android_serial = None):
+        if android_serial is None:
+            self.device = device
+        else:
+            self.device = Device(android_serial)
 
-    def print_info(self):
-        logger.info('Device info: %s' % self.device.info)
+    def get_info(self):
+        """
+        Retrieve the device info
+        """
+        return self.device.info
 
-    def get_object(self,**stuff):
-        return self.device(**stuff)
+#Key Event Actions of the device
+    """
+    Turn on/off screen
+    """
+    def turn_on_screen(self):
+        """
+        Turn on screen
+        """
+        self.device.screen.on()
+
+    def turn_off_screen(self):
+        """
+        Turn off screen
+        """
+        self.device.screen.off()
+
+    def wakeup_the_device(self):
+        """
+        wakeup the device
+        """
+        self.device.wakeup()
+
+    def sleep(self):
+        """
+        sleep the device, same as turning off the screen.
+        """
+        self.device.sleep()
+
+    """
+    Press hard/soft key
+    """
+
+    def press_key(self, *key):
+        """
+        press *key* keycode
+        """
+        self.device.press(*key)
+
+    def press_home(self):
+        """
+        press home key
+        """
+        self.device.press.home()
+
+    def press_back(self):
+        """
+        press back key
+        """
+        self.device.press.back()
+
+    def press_left(self):
+        """
+        press left key
+        """
+        self.device.pres.left()
+
+    def press_right(self):
+        """
+        press right key
+        """
+        self.device.press.right()
+
+    def press_up(self):
+        """
+        press up key
+        """
+        self.device.press.up()
+
+    def press_down(self):
+        """
+        press down key
+        """
+        self.device.press.down()
+
+    def press_center(self):
+        """
+        press center key
+        """
+        self.device.press.center()
+
+    def press_menu(self):
+        """
+        press menu key
+        """
+        self.device.press.menu()
+
+    def press_search(self):
+        """
+        press search key
+        """
+        self.device.press.search()
+
+    def press_enter(self):
+        """
+        press enter key
+        """
+        self.device.press.enter()
+
+    def press_delete(self):
+        """
+        press delete key
+        """
+        self.device.press.delete()
+
+    def press_recent(self):
+        """
+        press recent key
+        """
+        self.device.press.recent()
+
+    def press_volume_up(self):
+        """
+        press volume up key
+        """
+        self.device.press.volume_up()
+
+    def press_volume_down(self):
+        """
+        press volume down key
+        """
+        self.device.press.volume_down()
+
+    def press_camera(self):
+        """
+        press camera key
+        """
+        self.device.press.camera()
+
+    def press_power(self):
+        """
+        press power key
+        """
+        self.device.press.power()
+
+#Gesture interaction of the device
+#Screen Actions of the device
+#Watcher
+#     def (self):
+#         """
+#          
+#         """
+#         self.device
+#Selector
+
+    def get_object(self, **attribute):
+        """
+        get the ui object with attribute *attribute*
+        """
+        return self.device(**attribute)
 
     def get_info_of_object(self, obj):
         return obj.info
 
-    def press_power(self):
-        self.device.press.power()
+    def click_on(self, **attribute):
+        """
+        click on the object with *attribute*
+        """
+        self.device(**attribute).click()
 
-    def press_home(self):
-        self.device.press.home()
-
-    def click(self, **stuff):
-        self.device(**stuff).click()
-
-    def call(self, obj, method, **stuff):
+    def call(self, obj, method, **attribute):
         func = getattr(obj, method)
-        return func(**stuff)
+        return func(**attribute)
 
 if __name__ == '__main__':
     print 'start'
 
     m = Mobile('HYZPLVR48T6TS8LV')
-    linearLayout = m.get_object(className='android.widget.LinearLayout', index=2)
-    info_dict = m.get_info_of_object(linearLayout)
-    print info_dict
-    print info_dict['chileCount']
-    chileCount = int(info_dict['chileCount']) - 1
-    print chileCount
-    btn = m.call(linearLayout, 'child', instance=chileCount, className='android.widget.ImageView')
-    m.call(btn, 'click')
+
+    m.press_home()
+
+#     m.turn_off_screen()
+#     m.turn_on_screen()
+#     m.sleep()
+#     m.wakeup_the_device()
+    
+#     linearLayout = m.get_object(className='android.widget.LinearLayout', index=2)
+#     info_dict = m.get_info_of_object(linearLayout)
+#     print info_dict
+#     print info_dict['chileCount']
+#     chileCount = int(info_dict['chileCount']) - 1
+#     print chileCount
+#     btn = m.call(linearLayout, 'child', instance=chileCount, className='android.widget.ImageView')
+#     m.call(btn, 'click')
 
     print 'end'
