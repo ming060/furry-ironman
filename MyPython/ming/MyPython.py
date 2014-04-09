d45bade8d816c431b5177c6ffb2667dcb438ed81
ming060 <lym060@gmail.com>
2014-04-10 00:18:07 +0800

    python uiautomator example

--- MyPython/ming/MyPython.py
+++ MyPython/ming/MyPython.py
@@ -3,7 +3,14 @@ Created on 2014/3/31
 
 @author: YuMing
 '''
+from uiautomator import device as d
 
 if __name__ == '__main__':
     print 'start'
+    
+    d.press.home()
+    d(description='Apps').click()
+    d(text='Apps').click()
+    
+    
     print 'end'
