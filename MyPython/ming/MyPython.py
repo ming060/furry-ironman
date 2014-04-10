f321b275f1b78d90c09f94baa27065a6b122bdf4
ming060 <lym060@gmail.com>
2014-04-10 15:31:49 +0800

    finish examples

--- MyPython/ming/MyPython.py
+++ MyPython/ming/MyPython.py
@@ -8,9 +8,11 @@ from uiautomator import device as d
 if __name__ == '__main__':
     print 'start'
     
-    d.press.home()
-    d(description='Apps').click()
-    d(text='Apps').click()
+#     print d(text="Settings").wait.exists(timeout=3000)
+    # wait until the ui object gone
+    #true means the object does not exist
+    #false means the object still exist
+    print d(text="Settings").wait.gone(timeout=5000)
     
     
     print 'end'
