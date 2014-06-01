ddfb6eac0418eb26aad644f11466f9c2417dbdbb
ming060 <lym060@gmail.com>
2014-06-01 21:53:56 +0800

    dist

--- robotframework-uiautomatorlibrary/setup.py
+++ robotframework-uiautomatorlibrary/setup.py
@@ -3,7 +3,19 @@ from distutils.core import setup
 setup(
     name = 'robotframework-uiautomatorlibrary',
     packages = ['robotframework-uiautomatorlibrary'],
-    version = '0.1',
+    version = '0.2',
+    author='ming060',
+    author_email = 'lym060@gmail.com',
+    url = 'https://github.com/ming060/robotframework-uiautomatorlibrary',
     description = 'Robot Framework Android Test Library',
-    author='Ming060'
+    long_description = '',
+    install_requires = ['uiautomator >= 0.1.28'],
+    classifiers  = [
+                    'Programming Language :: Python :: 2.7',
+#                     'License :: ', #TODO
+                    'Operating System :: Microsoft :: Windows :: Windows 7',
+                    'Development Status :: 4 - Beta',
+                    'Intended Audience :: Developers',
+                    'Topic :: Software Development :: Testing'
+                    ]
 )
