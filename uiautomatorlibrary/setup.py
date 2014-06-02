5fb6eeb20ee777a9811ee569314f61db652e4a08
ming060 <lym060@gmail.com>
2014-06-02 16:41:59 +0800

    add project

---
+++ uiautomatorlibrary/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name = 'robotframework-uiautomatorlibrary',
+    packages = ['uiautomatorlibrary'],
+    version = '0.2',
+    author='ming060',
+    author_email = 'lym060@gmail.com',
+    url = 'https://github.com/ming060/robotframework-uiautomatorlibrary',
+    description = 'Robot Framework Android Test Library',
+    long_description = '',
+    install_requires = ['uiautomator >= 0.1.28'],
+    classifiers  = [
+                    'Programming Language :: Python :: 2.7',
+#                     'License :: ', #TODO
+                    'Operating System :: Microsoft :: Windows :: Windows 7',
+                    'Development Status :: 4 - Beta',
+                    'Intended Audience :: Developers',
+                    'Topic :: Software Development :: Testing'
+                    ]
+)
---
+++ uiautomatorlibrary/uiautomatorlibrary/__init__.py
@@ -0,0 +1,24 @@
+from Mobile import Mobile
+
+class uiautomatorlibrary(Mobile):
+    """
+    robotframework-uiautomatorlibrary is an Android device testing library for Robot Framework.
+
+    It uses uiautomator - Python wrapper for Android uiautomator tool (https://pypi.python.org/pypi/uiautomator/0.1.28) internally.
+
+    *Before running tests*
+
+    You can use `Set Serial` to specify which device to perform the test.
+
+
+    *Identify UI object*
+
+    There are two kinds of keywords.
+
+    
+    """
+
+    __version__ = '0.1'
+    ROBOT_LIBRARY_DOC_FORMAT = 'ROBOT'
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
+    pass
