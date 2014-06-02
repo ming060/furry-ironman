3861099c6084e1e8f95d9ce18d5ba90e1ba406da
ming060 <lym060@gmail.com>
2014-06-02 16:23:48 +0800

    delete test dir

--- uiautobot/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name = 'uiautobot',
-    packages = ['uiautobot'],
-    version = '1.0',
-    description = 'Robot Framework Android Test Library',
-    author='Ming060'
-)
